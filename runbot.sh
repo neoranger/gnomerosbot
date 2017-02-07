de302add12fc690098560b3e696fd348910a6a66
neoranger <neoranger@openmailbox.org>
2017-02-07 14:49:52 +0000

    new file:   runbot.sh

---
+++ runbot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while true
+do
+python gnomerosbot.py
+echo "¡The bot is crashed!"
+echo "Rebooting in:"
+for i in 1
+do
+echo "$i..."
+done
+echo "###########################################"
+echo "#Bot is restarting now                    #"
+echo "###########################################"
+done
